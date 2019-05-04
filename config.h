a6f40b800153dffec111d84782c91bc8da21b548
Merlin Buczek <merlin.buczek@pm.me>
2019-05-04 10:56:20 +0200

    color settings

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -85,37 +85,33 @@ unsigned int tabspaces = 8;
 /* bg opacity */
 unsigned int alpha = 0xed;
 
+/* Monokai colors */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
-	"black",   /* 256 -> bg */
-	"white",   /* 257 -> fg */
+  "#1d1f21", /* base00 */
+  "#f92672", /* base08 */
+  "#a6e22e", /* base0B */
+  "#f4bf75", /* base0A */
+  "#66d9ef", /* base0D */
+  "#ae81ff", /* base0E */
+  "#a1efe4", /* base0C */
+  "#f8f8f2", /* base05 */
+  "#75715e", /* base03 */
+  "#fd971f", /* base09 */
+  "#383830", /* base01 */
+  "#49483e", /* base02 */
+  "#a59f85", /* base04 */
+  "#f5f4f1", /* base06 */
+  "#cc6633", /* base0F */
+  "#f9f8f5", /* base07 */
 };
-
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 15;
+unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
+static unsigned int defaultcs = 13;
 static unsigned int defaultrcs = 0;
 
 /*
@@ -125,7 +121,7 @@ static unsigned int defaultrcs = 0;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
